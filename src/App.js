05c0bc22423f4708610e76c201e6d2ee3148af19
Barry McGee <barry.mcgee@integralcode.co.uk>
2017-09-28 16:13:14 +0100

    Update index.js with simple info

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Vanilla Framework (in React)</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a simple implementation of Vanilla Framework using React.
         </p>
       </div>
     );

5a7f0cf3ca66627b6f452d95ec9776311c5a122a
Barry McGee <barry.mcgee@integralcode.co.uk>
2017-10-06 16:49:52 +0100

    Add comment

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -3,6 +3,7 @@ import React from 'react';
 class Switch extends React.Component {
   render() {
     return (
+      // Why are styled not being applied to this component?
       <button className="p-switch" id="switch-on" type="button" role="switch" aria-checked="true" aria-labelledby="switch-on-label">
         <span>On</span>
         <span>Off</span>

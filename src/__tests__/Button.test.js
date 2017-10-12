c5120ced6527a033f4adfa99bdd11fa5e487cecb
Barry McGee <barry.mcgee@integralcode.co.uk>
2017-10-12 13:13:41 +0100

    Move components test into local folder

--- src/__tests__/Button.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '../components/Button/Button';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button value="Submit" />, div);
-});
-
---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+import Button from './Button';
+
+describe('Button component should render expected markup', () => {
+  it('should compare the component with a snapshot', () => {
+    const button = ReactTestRenderer.create(<Button value="Submit" />);
+    const json = button.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+});
+

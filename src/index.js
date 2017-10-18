74fff6939760e27358879775243603839bbd8ad7
Barry McGee <barry.mcgee@integralcode.co.uk>
2017-10-18 14:27:08 +0100

    Remove unused import from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';

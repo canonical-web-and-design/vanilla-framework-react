872e48b051b474274bd9a9dd4b69dcc67bc733d3
Anthony Dillon <me@anthonydillon.com>
2017-11-06 17:19:44 +0000

    Add copy snippet functionality

--- src/components/CodeSnippet/CodeSnippet.js
+++ src/components/CodeSnippet/CodeSnippet.js
@@ -3,11 +3,37 @@ import PropTypes from 'prop-types';
 import './CodeSnippet.css';
 
 class CodeSnippet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleInputClick = this.handleInputClick.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleInputClick() {
+    this.textInput.select();
+  }
+
+  handleButtonClick() {
+    this.textInput.select();
+    try {
+      document.execCommand('copy');
+    } catch(err) {
+      console.warn('Unable to copy');
+    }
+  }
+
   render() {
     return (
       <div className="p-code-snippet">
-        <input className="p-code-snippet__input" value={this.props.value} readOnly="readonly" />
-        <button className="p-code-snippet__action">Copy to clipboard</button>
+        <input
+          className="p-code-snippet__input"
+          value={this.props.value}
+          readOnly="readonly"
+          onClick={this.handleInputClick}
+          ref={(input) => { this.textInput = input; }} />
+        <button
+          className="p-code-snippet__action"
+          onClick={this.handleButtonClick}>Copy to clipboard</button>
       </div>
     );
   }

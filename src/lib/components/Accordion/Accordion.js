04d734cfab079edbb8c626e05d8fc1d644ffc23e
Caleb Ellis <caleb.ellis@canonical.com>
2017-11-13 15:36:08 +0000

    43: Fixed Accordion component to accept custom text

--- src/lib/components/Accordion/Accordion.js
+++ src/lib/components/Accordion/Accordion.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import AccordionItem from './AccordionItem';
+import PropTypes from 'prop-types';
 import './Accordion.css';
 
-class Accordion extends React.Component {
-  render() {
-    return (
-      <aside className="p-accordion" role="tablist">
-        <ul className="p-accordion__list">
-          <AccordionItem title="Title of Item 1">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
-          <AccordionItem title="Title of Item 2">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
-          <AccordionItem title="Title of Item 3">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
-        </ul>
-      </aside>
-    );
-  }
-}
+const Accordion = props => (
+  <aside className="p-accordion" role="tablist">
+    <ul className="p-accordion__list">
+      { props.children }
+    </ul>
+  </aside>
+);
+
+Accordion.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Accordion;

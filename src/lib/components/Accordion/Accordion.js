f0a8e63e3338ad7a27a8b68647fbc2dafd935ce3
Caleb Ellis <caleb.ellis@canonical.com>
2017-11-14 09:38:34 +0000

    35: Updated Accordion and AccordionItem logic

--- src/lib/components/Accordion/Accordion.js
+++ src/lib/components/Accordion/Accordion.js
@@ -1,34 +1,46 @@
 import React from 'react';
-import AccordionItem from './AccordionItem';
+import PropTypes from 'prop-types';
 import './Accordion.css';
 
 class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleSection = this.toggleSection.bind(this);
+    this.state = {
+      open: props.selected,
+    };
+  }
+
+  toggleSection(index) {
+    this.setState({ open: this.state.open === index ? null : index });
+  }
+
   render() {
+    const accordionItems = React.Children.map(this.props.children,
+      (child, index) => React.cloneElement(child, {
+        index,
+        isOpen: this.state.open === index,
+        onClick: this.toggleSection,
+      }),
+    );
+
     return (
       <aside className="p-accordion" role="tablist">
         <ul className="p-accordion__list">
-          <AccordionItem title="Title of Item 1">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
-          <AccordionItem title="Title of Item 2">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
-          <AccordionItem title="Title of Item 3">
-            <p>Lorem Ipsum has been the industrys standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book.
-            </p>
-          </AccordionItem>
+          { accordionItems }
         </ul>
       </aside>
     );
   }
 }
+
+Accordion.defaultProps = {
+  selected: null,
+};
+
+Accordion.propTypes = {
+  children: PropTypes.node.isRequired,
+  selected: PropTypes.number,
+};
 
 export default Accordion;

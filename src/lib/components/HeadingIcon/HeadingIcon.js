2fac640043667ecbfeac438ed242ab6efbf6b164
Caleb Ellis <caleb.ellis@canonical.com>
2017-11-14 16:20:42 +0000

    48: Add HeadingIcon component

---
+++ src/lib/components/HeadingIcon/HeadingIcon.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './HeadingIcon.css';
+
+const HeadingIcon = props => (
+  <div className="p-heading-icon">
+    <div className="p-heading-icon__header">
+      <img className="p-heading-icon__img" alt={props.alt} src={props.src} />
+      <h3 className="p-heading-icon__title">{props.title}</h3>
+    </div>
+    {props.children}
+  </div>
+);
+
+HeadingIcon.defaultProps = {
+  alt: '',
+  children: '',
+};
+
+HeadingIcon.propTypes = {
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default HeadingIcon;

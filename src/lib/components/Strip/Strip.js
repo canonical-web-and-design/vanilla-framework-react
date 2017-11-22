1249b70331cb066b6b840ca621af755a3308cf38
Caleb Ellis <caleb.ellis@canonical.com>
2017-11-22 17:04:13 +0000

    59: Add Strip component

---
+++ src/lib/components/Strip/Strip.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Strip.css';
+
+const Strip = (props) => {
+  let classArray = [];
+  let style = Object.assign({}, props.style);
+  const {
+    colour, image, bordered, padding, children,
+  } = props;
+
+  if (colour) {
+    classArray = [...classArray, `p-strip--${props.colour}`];
+  }
+
+  if (image.src) {
+    const imageColour = `is-${image.colour}`;
+    classArray = [...classArray, 'p-strip--image', imageColour];
+    style = Object.assign(
+      style, { backgroundImage: `url(${image.src})` },
+    );
+  }
+
+  if (bordered) {
+    classArray = [...classArray, 'is-bordered'];
+  }
+
+  if (padding) {
+    classArray = [...classArray, `is-${padding}`];
+  }
+
+  const classString = classArray.join(' ');
+
+  return (
+    <section
+      className={classString}
+      style={style}
+    >
+      {children}
+    </section>
+  );
+};
+
+Strip.defaultProps = {
+  colour: 'light',
+  bordered: false,
+  padding: null,
+  image: {
+    src: '',
+    colour: 'dark',
+  },
+  style: {},
+};
+
+Strip.propTypes = {
+  children: PropTypes.node.isRequired,
+  colour: PropTypes.oneOf(['light', 'dark', 'accent']),
+  image: PropTypes.shape({
+    src: PropTypes.string,
+    colour: PropTypes.oneOf(['light', 'dark']),
+  }),
+  bordered: PropTypes.bool,
+  padding: PropTypes.oneOf(['shallow', 'deep']),
+  style: PropTypes.object,
+};
+
+export default Strip;

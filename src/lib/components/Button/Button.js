67e0927e50d82822a30dbace9434c34a6db24291
Caleb Ellis <caleb.ellis@canonical.com>
2017-12-04 13:50:01 +0000

    77: Update Button component props logic and snapshot tests

--- src/lib/components/Button/Button.js
+++ src/lib/components/Button/Button.js
@@ -3,18 +3,62 @@ import PropTypes from 'prop-types';
 import './Button.scss';
 
 class Button extends React.Component {
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this);
+    this.getClassString = this.getClassString.bind(this);
+  }
+
+  onClick(event) {
+    if (!this.props.disabled) {
+      this.props.onClick(event);
+    }
+  }
+
+  getClassString() {
+    let classString = 'p-button--base';
+
+    if (this.props.positive) classString = 'p-button--positive';
+    else if (this.props.negative) classString = 'p-button--negative';
+    else if (this.props.brand) classString = 'p-button--brand';
+    else if (this.props.neutral) classString = 'p-button--neutral';
+    if (this.props.inline) classString = `${classString} is-inline`;
+
+    return classString;
+  }
+
   render() {
     return (
-      <button className={`p-button--${this.props.modifier}`}>{this.props.value}</button>
+      <button
+        className={this.getClassString()}
+        onClick={this.props.onClick}
+        disabled={this.props.disabled}
+      >
+        {this.props.value}
+      </button>
     );
   }
 }
 
-Button.defaultProps = { modifier: 'base' };
+Button.defaultProps = {
+  neutral: false,
+  brand: false,
+  negative: false,
+  positive: false,
+  disabled: false,
+  inline: false,
+  onClick: () => null,
+};
 
 Button.propTypes = {
-  modifier: PropTypes.string,
   value: PropTypes.string.isRequired,
+  neutral: PropTypes.bool,
+  brand: PropTypes.bool,
+  negative: PropTypes.bool,
+  positive: PropTypes.bool,
+  inline: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 Button.displayName = 'Button';
--- src/lib/components/Button/Button.stories.js
+++ src/lib/components/Button/Button.stories.js
@@ -6,27 +6,39 @@ import Button from './Button';
 
 storiesOf('Buttons', module)
   .add('Base',
-    withInfo('A base button is usually used alongside a neutral button.')(() =>
+    withInfo('A base button is usually used alongside a neutral button. Precedence for button type is: base < neutral < brand < negative < positive.')(() =>
       <Button value="Base button 😶" />,
     ),
   )
   .add('Neutral',
     withInfo('A neutral button can be used to indicate a positive action that isn\'t necessarily the main call-to-action.')(() =>
-      <Button value="Neutral button 😐" modifier="neutral" />,
+      <Button neutral value="Neutral button 😐" />,
     ),
   )
   .add('Brand',
     withInfo('You can use the brand button with the main color of your brand.')(() =>
-      <Button value="Brand button 💥" modifier="brand" />,
+      <Button brand value="Brand button 💥" />,
     ),
   )
   .add('Negative',
     withInfo('A negative button can be used to indicate a negative action that is destructive or permanent.')(() =>
-      <Button value="Negative button 😡" modifier="negative" />,
+      <Button negative value="Negative button 😡" />,
     ),
   )
   .add('Positive',
     withInfo('A positive button can be used to indicate a positive action that is the main call-to-action.')(() =>
-      <Button value="Positive button 😁" modifier="positive" />,
+      <Button positive value="Positive button 😁" />,
+    ),
+  )
+  .add('Inline',
+    withInfo('Should you wish to place a button after a line of inline text, as a CTA for example, you can do so by adding the prop "inline" to the Button component.')(() => (
+      <p>
+        This is an inline button. <Button neutral inline value="Inline button" />
+      </p>),
+    ),
+  )
+  .add('Disabled',
+    withInfo('You can disable a button by adding the "disabled" prop.')(() =>
+      <Button disabled value="Disabled button" />,
     ),
   );

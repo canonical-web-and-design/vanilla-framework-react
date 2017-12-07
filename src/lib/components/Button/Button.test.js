745d51092d614a7d7d00179572e2364713379212
Caleb Ellis <calebellis2805@gmail.com>
2017-12-07 14:46:44 +0000

    77: Update Button component props logic and snapshot tests (#83)

--- src/lib/components/Button/Button.test.js
+++ src/lib/components/Button/Button.test.js
@@ -3,8 +3,23 @@ import ReactTestRenderer from 'react-test-renderer';
 import Button from './Button';
 
 describe('Button component', () => {
-  it('should compare with a snapshot', () => {
-    const button = ReactTestRenderer.create(<Button value="Submit" />);
+  it('should render a simple Button correctly', () => {
+    const button = ReactTestRenderer.create(
+      <Button value="Submit" />);
+    const json = button.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+
+  it('should render a modified Button correctly', () => {
+    const button = ReactTestRenderer.create(
+      <div>
+        <Button neutral value="Neutral" />
+        <Button negative value="Negative" />
+        <Button positive value="Positive" />
+        <Button brand value="Brand" />
+        <Button inline value="Inline" />
+        <Button disabled value="Disabled" />
+      </div>);
     const json = button.toJSON();
     expect(json).toMatchSnapshot();
   });

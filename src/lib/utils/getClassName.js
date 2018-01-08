977f778a89cfac13b51f94ffe70a539b9dd771cb
Caleb Ellis <caleb.ellis@canonical.com>
2018-01-08 17:13:24 +0000

    112: Add extra unit tests to Button

--- src/lib/utils/getClassName.js
+++ src/lib/utils/getClassName.js
@@ -1,6 +1,6 @@
 export default function getClassName(classesObj) {
-  const classNames = Object.keys(classesObj).filter(key => classesObj[key]);
-  const classString = classNames.join(' ');
-
-  return classString;
+  return Object
+    .keys(classesObj)
+    .filter(key => classesObj[key])
+    .join(' ');
 }

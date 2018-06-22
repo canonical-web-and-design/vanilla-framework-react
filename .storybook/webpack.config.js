165a8b159b1279f813fb466c5c9dd9527cf2d225
Caleb Ellis <caleb.ellis@canonical.com>
2018-06-22 16:13:08 +0100

    Minor code fixes

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,8 +5,7 @@ module.exports = (storybookBaseConfig, env, defaultConfig) => {
   const port = process.env.PORT || 8102;
   storybookBaseConfig.plugins.push(new FriendlyErrorsPlugin({
     compilationSuccessInfo: {
-      messages: [`Your application is running here: http://${host}:${port}
-      `], // Intentional new line to pad output
+      messages: [`Your application is running here: http://${host}:${port}\n`],
     },
   }));
   storybookBaseConfig.devServer = {quiet: true};

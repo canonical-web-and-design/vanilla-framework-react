83c383316904cf7399186c1a6f64f6b3885da41d
Caleb Ellis <caleb.ellis@canonical.com>
2018-06-22 11:03:02 +0100

    Remove unnecessary webpack configs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,30 +1,15 @@
-'use strict'
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const devConfig = require('../config/webpack.config.dev')
-const prodConfig = require('../config/webpack.config.prod')
 
-module.exports = (storybookBaseConfig, configType) => {
-  let appConfig = prodConfig;
-  if (configType === 'DEVELOPMENT') {
-    const appConfig = devConfig;
-  }
-  storybookBaseConfig.module.rules = [
-    ...storybookBaseConfig.module.rules,
-    ...appConfig.module.rules,
-  ];
-  storybookBaseConfig.resolve = appConfig.resolve;
-
-  if (configType === 'DEVELOPMENT') {
-    const host = process.env.HOST || 'localhost';
-    const port = process.env.PORT || 6006;
-    storybookBaseConfig.plugins.push(new FriendlyErrorsPlugin({
-      compilationSuccessInfo: {
-        messages: [`Your application is running here: http://${host}:${port}
-        `], // Intentional new line to pad output
-      },
-    }));
-    storybookBaseConfig.devServer = {quiet: true};
-  }
+module.exports = (storybookBaseConfig, env, defaultConfig) => {
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 8102;
+  storybookBaseConfig.plugins.push(new FriendlyErrorsPlugin({
+    compilationSuccessInfo: {
+      messages: [`Your application is running here: http://${host}:${port}
+      `], // Intentional new line to pad output
+    },
+  }));
+  storybookBaseConfig.devServer = {quiet: true};
 
   return storybookBaseConfig;
 };

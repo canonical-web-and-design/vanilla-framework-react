495157cd266dc2ecbcb9266d072b455062e7ae8c
Caleb Ellis <caleb.ellis@canonical.com>
2018-07-17 16:49:51 +0100

    Convert Switch component to checkbox version

--- src/lib/components/Switch/Switch.js
+++ src/lib/components/Switch/Switch.js
@@ -1,45 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Switch extends React.Component {
-  constructor() {
-    super();
-    this.switchToggle = this.switchToggle.bind(this);
-    this.state = {
-      checked: false,
-    };
-  }
+const Switch = (props) => {
+  const { checked, id, label } = props;
 
-  switchToggle() {
-    const { checked } = this.state;
-    this.setState({ checked: !checked });
-  }
+  return (
+    <label htmlFor={id}>
+      {label}
+      <input
+        id={id}
+        type="checkbox"
+        className="p-switch"
+        value={checked ? 'on' : 'off'}
+        checked={checked}
+      />
+      <div className="p-switch__slider" />
+    </label>
+  );
+};
 
-  render() {
-    const { checked } = this.state;
-    return (
-      <span>
-        <label id="switch-on-label" htmlFor="switch-on">{ this.props.label }
-          <button
-            id="switch-on"
-            className="p-switch"
-            type="button"
-            role="switch"
-            aria-checked={checked}
-            aria-labelledby="switch-on-label"
-            onClick={() => this.switchToggle(this)}
-          >
-            <span>On</span>
-            <span>Off</span>
-          </button>
-        </label>
-      </span>
-    );
-  }
-}
+Switch.defaultProps = {
+  checked: false,
+  label: undefined,
+};
 
 Switch.propTypes = {
-  label: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
 Switch.displayName = 'Switch';
